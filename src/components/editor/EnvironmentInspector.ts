a0307bd4629c473f62e882b39f7eaa948dcddf4c
samba <samba@manjaro>
2022-05-08 17:36:13 +0200

    removing chorreva

--- src/components/editor/EnvironmentInspector.ts
+++ src/components/editor/EnvironmentInspector.ts
@@ -2,7 +2,6 @@ import { EnvironmentType, IEnvironmentInspector, MidiDeviceInfo } from "@werckme
 import _ = require("lodash");
 
 export const SoundFontRepoMap = {
-    "choriumreva": "https://raw.githubusercontent.com/werckme/soundfont-server/main/soundfonts/choriumreva/choriumreva.sf2.json",
     "FluidR3-GM": "https://raw.githubusercontent.com/werckme/soundfont-server/main/soundfonts/FluidR3_GM_EX/FluidR3_GM_EX.sf2.json",
     "Live-HQ-Natural-SoundFont-GM": "https://raw.githubusercontent.com/werckme/soundfont-server/main/soundfonts/Live_HQ_Natural_SoundFont_GM/Live_HQ_Natural_SoundFont_GM.sf2.json",
 };
